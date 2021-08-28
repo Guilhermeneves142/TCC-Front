f457c12e70f27246f290d6f5d09940962b5d449d
Guilhermeneves142 <guilherme_n2010@hotmail.com>
2021-08-27 21:40:24 -0300

    fix: scale ticket nao abre quando dispatcher

--- src/components/modals/Paciente/PacienteForm.vue
+++ src/components/modals/Paciente/PacienteForm.vue
@@ -44,11 +44,74 @@
                 </v-col>
               </v-row>
               <v-row>
-                <v-col cols="12"> </v-col>
+                <v-col cols="4">
+                  <v-menu
+                    v-model="data"
+                    :close-on-content-click="false"
+                    max-width="290"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field
+                        :value="formatDateToString(paciente.dataNascimento)"
+                        label="Data de nascimento *"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker
+                      v-model="paciente.dataNascimento"
+                      locale="pt-br"
+                      :title-date-format="formatDateToString"
+                      @change="data = false"
+                      year-icon="mdi-calendar-blank"
+                    />
+                  </v-menu>
+                </v-col>
+                <v-col cols="4">
+                  <v-text-field
+                    label="Telefone"
+                    v-model="paciente.telefone"
+                    prefix="(55)"
+                  />
+                </v-col>
+                <v-col cols="4">
+                  <v-text-field
+                    label="Celular *"
+                    v-model="paciente.celular"
+                    prefix="(55)"
+                  />
+                </v-col>
               </v-row>
+              <v-row>
+                <v-col cols="3">
+                  <v-radio-group
+                    v-model="paciente.genero"
+                    label="Genero *"
+                    mandatory
+                  >
+                    <v-radio label="Masculino" value="masculino" />
+                    <v-radio label="Feminino" value="Feminino" />
+                  </v-radio-group>
+                </v-col>
+                <v-col cols="5">
+                  <v-select
+                    label="Responsavel"
+                    no-data-text="Sem dados disponiveis"
+                    v-model="paciente.responsavel"
+                    :items="objetivos"
+                    item-text="nome"
+                    clearable
+                    item-value="id"
+                    return-object
+                  />
+                </v-col>
+              </v-row>
+              <v-row> </v-row>
             </v-container>
           </v-form>
           <small>*Campos obrigatórios</small>
+          <responsavel-form />
         </v-card-text>
         <v-card-actions>
           <v-spacer />
@@ -83,13 +146,20 @@
 </template>
 
 <script lang="ts">
-import ObjectiveService from "@/services/ObjectiveService";
 import { Component, Vue } from "vue-property-decorator";
+import ResponsavelForm from "./ResponsavelForm.vue";
+import ObjectiveService from "@/services/ObjectiveService";
+import Moment from "moment";
 
-@Component
+@Component({
+  components: {
+    ResponsavelForm,
+  },
+})
 export default class PacienteForm extends Vue {
   dialog = true;
   loading = false;
+  data = false;
   valid = false;
   notification = {
     open: false,
@@ -111,6 +181,11 @@ export default class PacienteForm extends Vue {
   };
   objetivos: Objective.Objective[] = [];
 
+  async created() {
+    this.paciente.dataNascimento =
+      Moment.utc(969159600000).format("YYYY-MM-DD");
+  }
+
   async mounted() {
     this.objetivos = await ObjectiveService.findAll();
   }
@@ -119,6 +194,10 @@ export default class PacienteForm extends Vue {
     return {
       nome: [(v: string) => !!v || "Nome é obrigatório"],
     };
+  }
+
+  formatDateToString(data: string) {
+    return Moment(data).locale("pt-br").format("DD MMM YYYY");
   }
 
   close() {
--- src/components/modals/Paciente/ResponsavelForm.vue
+++ src/components/modals/Paciente/ResponsavelForm.vue
@@ -1,12 +1,43 @@
 <template>
-  <div></div>
+  <v-expansion-panels flat>
+    <v-expansion-panel>
+      <v-expansion-panel-header hide-actions>
+        <template v-slot:default="{ open }">
+          <v-row no-gutters>
+            <v-col cols="7">
+              <span v-if="open">Responsavel</span>
+            </v-col>
+
+            <v-col cols="3">
+              <v-btn color="primary" dark class="mt-3">
+                Criar novo Responsavel
+              </v-btn>
+            </v-col>
+          </v-row>
+        </template>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-text-field
+          v-model="trip.name"
+          placeholder="Caribbean Cruise"
+        ></v-text-field>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class ResponsavelForm extends Vue {}
+export default class ResponsavelForm extends Vue {
+  trip = {
+    name: "",
+    location: null,
+    start: null,
+    end: null,
+  };
+}
 </script>
 
 <style scoped></style>
